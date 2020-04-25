af160fa4bfe720afdaae0f41fe16d3f9f0e0a645
GokhanYilmaz <itsgokhanyilmaz@gmail.com>
2020-04-26 02:10:07 +0300

    Initial commit

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/version")
+public class HomeController {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping
+    public Map getStatus(){
+        Map map = new HashMap<String,String>();
+        map.put("app-version", appVersion);
+        return map;
+    }
+}
